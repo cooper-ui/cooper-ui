7a0a5873479c3e96bf71d3e054a0207779832f5e
Angel Romero <angel@froged.com>
2022-08-12 21:49:00 +0200

    initial commit

---
+++ apps/cooper-playbook/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cooper-ui-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'cooper-playbook';
+}
