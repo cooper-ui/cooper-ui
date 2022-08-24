c9fe5940023ea35be9fe80676e45d41b4d6653bf
Angel Romero <angel@froged.com>
2022-08-24 20:33:40 +0200

    Fix some styles

--- libs/cooper-ui/src/components/root/root.component.ts
+++ libs/cooper-ui/src/components/root/root.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, Input, Renderer2 } from '@angular/core';
 
 @Component({
     selector: 'cui-root',
@@ -6,4 +6,26 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
     styleUrls: ['./root.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CuiRootComponent { }
+export class CuiRootComponent {
+
+    constructor(
+        private elementRef: ElementRef,
+        private renderer: Renderer2,
+    ) { }
+
+    @Input('darkModeEnabled') set setDarkModeEnabled(value: boolean) {
+        this.darkModeEnabled = value;
+        this.setDarkModeClass();
+    }
+
+    private darkModeEnabled = true;
+
+    private setDarkModeClass(): void {
+        if (this.darkModeEnabled) {
+            this.renderer.addClass(this.elementRef.nativeElement, 'darkModeEnabled');
+        } else {
+            this.renderer.removeClass(this.elementRef.nativeElement, 'darkModeEnabled');
+        }
+    }
+
+}
